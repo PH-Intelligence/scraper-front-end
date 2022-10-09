36372c273cbe9fa22a0869a6ec6087c122a6e564
Jay Pinho <jaypinho@gmail.com>
2022-10-09 15:58:34 +0100

    Working authentication and gated access to more rows and exports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from "react";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -6,12 +7,17 @@ import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 import DataTable from './DataTable';
 
+import { supabase } from './supabaseClient'
+import './index.css'
+import Auth from './Auth'
+import Account from './Account'
+
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://twitter.com/jaypinho">
+        Jay Pinho
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -20,16 +26,39 @@ function Copyright() {
 }
 
 export default function App() {
+
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <Container maxWidth="md">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Employee Head Count and Job Openings
-        </Typography>
-        <DataTable />
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    <>
+      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+        {!session ? (
+          <Auth />
+        ) : (
+          <Account key={session.user.id} session={session} />
+        )}
+      </div>
+      <Container maxWidth="md">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Company Tracker
+          </Typography>
+          <DataTable logged_in={session} />
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </>
+  )
+
 }
--- src/DataTable.js
+++ src/DataTable.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from "react";
-import { DataGrid } from '@mui/x-data-grid';
-import { createClient } from '@supabase/supabase-js'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { supabase } from './supabaseClient'
 
 const columns = [
   // { field: 'id', headerName: 'ID' },
@@ -22,8 +22,10 @@ const columns = [
   { field: 'Companies_Tags', 
     headerName: 'Company Categories',
     width: 300,
+    valueGetter: (params) => {return params.row.Companies.Tags},
+    valueFormatter: ({value}) => value,
     renderCell: (params: GridRenderCellParams<String>) => (
-          params.row.Companies.Tags ? params.row.Companies.Tags.split(",").map(x => <React.Fragment key={x}><span style={{backgroundColor: '#c8f7ef'}}>{x}</span>&nbsp;</React.Fragment>) : ''
+          params.row.Companies.Tags ? params.row.Companies.Tags.split(",").map(x => <React.Fragment key={x}><span style={{backgroundColor: '#398a7c', borderRadius: '10px', padding: '0px 5px'}}>{x}</span>&nbsp;</React.Fragment>) : ''
     )
   }
 ]
@@ -34,39 +36,43 @@ export default function DataTable(props) {
 
   useEffect(() => {
 
+    console.log("hey");
+
     (async() => {
-      // your code using await goes here
-      const supabase = createClient(
-        'https://abhxwgvatqovevkjxlyy.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiaHh3Z3ZhdHFvdmV2a2p4bHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUwMDI1MTcsImV4cCI6MTk4MDU3ODUxN30.81UKLfbdZqsPco1SC8vi2nf6wJNWbCbn6Yv5Tkn_B-0'
-      )
+      
+      const { data, error } = await supabase.from('LinkedIn Jobs')
+        .select(`
+          id,
+          Employees,
+          "Job Openings",
+          Timestamp,
+          Companies (
+            Company,
+            Tags
+          )
+        `)
+        if (error)
+          console.log('Error occurred:', error)
+        else
+          if (JSON.stringify(data.sort((a, b) => a.Companies.Company.localeCompare(b.Companies.Company))) != JSON.stringify(tableData)) {
+            var from_existing_data = tableData.length > 0 ? tableData[0]['Companies'] : '';
+            var from_supabase = data.sort((a, b) => a.Companies.Company.localeCompare(b.Companies.Company))[0]['Companies'];
+            console.log('changing');
+            setTableData(data.sort((a, b) => a.Companies.Company.localeCompare(b.Companies.Company)))
+          }
+      })()
 
-    const { data, error } = await supabase.from('LinkedIn Jobs')
-      .select(`
-        id,
-        Employees,
-        "Job Openings",
-        Timestamp,
-        Companies (
-          Company,
-          Tags
-        )
-      `)
-      if (error)
-        console.log('Error occurred:', error)
-      else
-        setTableData(data.sort((a, b) => a.Companies.Company.localeCompare(b.Companies.Company)))
-    })()
-
-  }, [])
+  }, [ tableData, props.logged_in])
 
   return (
     <div style={{ height: 600, display: 'flex' }}>
       <DataGrid
-       rows={tableData}
-       columns={columns}
-       pageSize={12}
-     />
+        rows={tableData}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10,25,100]}
+        components={props.logged_in ? {Toolbar: GridToolbar}: {}}
+      />
     </div>
   );
 }
